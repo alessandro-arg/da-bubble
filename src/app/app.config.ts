d981587aa8da4a3acb5b6d14e1e9ec27896fbac4
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-19 17:36:54 +0200

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    provideStorage(() => getStorage()),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCLHb_97eGzn_QnM9MEeSUxQQd5GBaM-Vw',
+    authDomain: 'dabubble-db274.firebaseapp.com',
+    databaseURL:
+      'https://dabubble-db274-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'dabubble-db274',
+    storageBucket: 'dabubble-db274.firebasestorage.app',
+    messagingSenderId: '1083530171367',
+    appId: '1:1083530171367:web:96eb10747948bbb305e342',
+  },
+};
