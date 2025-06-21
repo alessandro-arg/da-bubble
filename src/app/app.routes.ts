a59c41105aeb94ab966c2a4626cb337d7a77891c
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-21 11:17:46 +0200

    pull request

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,22 +5,24 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ChooseYourAvatarComponent } from './components/register/choose-your-avatar/choose-your-avatar.component';
 
-
 export const routes: Routes = [
-    {
-        path: 'landingpage', component: LandingPageComponent,
-        canActivate: [authGuard], // Protected route
-    },
+  {
+    path: 'landingpage',
+    component: LandingPageComponent,
+    canActivate: [authGuard], // Protected route
+  },
 
-    {
-        path: 'login', component: LoginComponent
-    },
+  {
+    path: '',
+    component: LoginComponent,
+  },
 
-    {
-        path: 'register', component: RegisterComponent
-    }
-    ,
-    {
-        path: 'choose-your-avatar', component: ChooseYourAvatarComponent
-    }
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'choose-your-avatar',
+    component: ChooseYourAvatarComponent,
+  },
 ];
