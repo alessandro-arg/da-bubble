aa68fb41cbf8926729b6d25d59a33c993da1519b
Alessandro Argenziano <1603alessandro@gmail.com>
2025-06-23 20:55:34 +0200

    the UI of the thread component is ready, working on the database structure

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewInit, AfterViewChecked {
+  @ViewChild('chatContainer')
+  private chatContainer!: ElementRef<HTMLDivElement>;
+  private hasScrolledInitially = false;
 
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+
+  // Better for *ngFor
+  ngAfterViewChecked() {
+    if (!this.hasScrolledInitially) {
+      this.scrollToBottom();
+      this.hasScrolledInitially = true;
+    }
+  }
+
+  scrollToBottom() {
+    const el = this.chatContainer?.nativeElement;
+    if (el) {
+      el.scrollTop = el.scrollHeight;
+    }
+  }
 }
