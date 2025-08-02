10a4db23d429b89e3f442366d1e5b8859af4d06c
Hamidou <hamiduguinea@gmail.com>
2025-08-02 19:52:00 +0200

    feat: create Impressum component with structured content and routing

--- src/app/components/impressum/impressum.component.ts
+++ src/app/components/impressum/impressum.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
-export class ImpressumComponent {
+export class ImpressumComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+    // Initialization logic can be added here if needed
+  }
 
 }
