babc1070e56efc17c2c63a3da4c88ce21b0de88a
Hamidou <hamiduguinea@gmail.com>
2025-08-02 19:56:50 +0200

    feat: enhance Impressum component with viewport scrolling and module imports

--- src/app/components/impressum/impressum.component.ts
+++ src/app/components/impressum/impressum.component.ts
@@ -1,18 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
 export class ImpressumComponent implements OnInit {
-  constructor() { }
+  constructor(private ViewportScroller: ViewportScroller ) { }
   ngOnInit(): void {
-    // Initialization logic can be added here if needed
+    this.ViewportScroller.scrollToPosition([0, 0]);
   }
-
 }
