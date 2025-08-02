5b29e19e1b51e5908fc6ed22ada097636324940d
Hamidou <hamiduguinea@gmail.com>
2025-08-02 19:57:43 +0200

    fix: format constructor and improve documentation in ImpressumComponent

--- src/app/components/impressum/impressum.component.ts
+++ src/app/components/impressum/impressum.component.ts
@@ -12,7 +12,13 @@ import { ViewportScroller } from '@angular/common';
   styleUrl: './impressum.component.scss'
 })
 export class ImpressumComponent implements OnInit {
-  constructor(private ViewportScroller: ViewportScroller ) { }
+  constructor(private ViewportScroller: ViewportScroller) { }
+
+  /**
+   * Lifecycle hook that is called after Angular has initialized the component.
+   * This method scrolls the viewport to the top-left position ([0, 0]) upon initialization.
+   * It ensures that the user starts at the top of the page when the component is loaded.
+   */
   ngOnInit(): void {
     this.ViewportScroller.scrollToPosition([0, 0]);
   }
