c9717bf32a413afe09979e1e5c5ccb4822ccbb86
Alessandro Argenziano <1603alessandro@gmail.com>
2025-08-09 16:01:14 +0200

    privacy policy is now working great and the design is correct, also works in responsive

--- src/app/components/privacy-policy/privacy-policy.component.ts
+++ src/app/components/privacy-policy/privacy-policy.component.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ViewportScroller } from '@angular/common';
+import { MobileService } from '../../services/mobile.service';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss'
+  styleUrl: './privacy-policy.component.scss',
 })
 export class PrivacyPolicyComponent implements OnInit {
+  isMobile = false;
+  arrowHover = false;
 
-  constructor(private viewportScroller: ViewportScroller) { }
+  constructor(
+    private viewportScroller: ViewportScroller,
+    private mobileService: MobileService
+  ) {}
 
   /**
    * Lifecycle hook that is called after Angular has initialized the component.
@@ -20,5 +26,9 @@ export class PrivacyPolicyComponent implements OnInit {
    */
   ngOnInit(): void {
     this.viewportScroller.scrollToPosition([0, 0]);
+
+    this.mobileService.isMobile$.subscribe((isMobile) => {
+      this.isMobile = isMobile;
+    });
   }
 }
